3fea2cd6785a5fc2fd9a7da59a04ff7e63d2ca39
Olivia L <letendartolivia@gmail.com>
2024-01-24 12:16:53 +0100

    import the icon for supply component + complete the composant

--- src/components/Supply/Supply.jsx
+++ src/components/Supply/Supply.jsx
@@ -1,8 +1,46 @@
+import './Supply.css';
+import calories from '../../assets/calories-icon.png';
+import carbs from '../../assets/carbs-icon.png'
+import fat from '../../assets/fat-icon.png'
+import protein from '../../assets/protein-icon.png'
+
+
 export default function Supply() {
+    const data = [
+        {
+            name: "Calories",
+            number: '1930Kcal',
+            img: calories
+        },
+        {
+            name: "Proteines",
+            number: '155g',
+            img: protein
+        },
+        {
+            name: "Glucides",
+            number: '250g',
+            img: carbs
+        },
+        {
+            name: "Lipides",
+            number: '50g',
+            img: fat
+        },
+    ]
 
     return (
-        <>
-        <p>Supply</p>
-        </>
+        <section className="supplyParent">
+            {data.map((data, index) => (
+                <div key={index}>
+                <img src={data.img}
+                     alt=""/>
+                <div>
+                    <h2>{data.number}</h2>
+                    <h3>{data.name}</h3>
+                </div>
+                </div>
+            ))}
+        </section>
     )
 }
